c44d4065787e14d4eca7f7a07ba7b27c82deb00c
Lendesz <lendesz@gmail.com>
2020-11-16 11:27:47 +0100

    maj

--- ex1/js/script.js
+++ ex1/js/script.js
@@ -2,8 +2,8 @@ let btn = document.getElementById('btn');
 let res = document.getElementById('res');
 
 btn.onclick = function () {
-    let iNum1 = document.getElementById('num1').value;
-    let iNum2 = document.getElementById('num2').value;
+    let iNum1 = parseFloat(document.getElementById('num1').value);
+    let iNum2 = parseFloat(document.getElementById('num2').value);
     let resultat = multiply(iNum1,iNum2);
     res.innerHTML = resultat;
 }
@@ -11,5 +11,6 @@ btn.onclick = function () {
 function multiply(n1, n2) {
     return n1 * n2;
 }
+
 
 
--- ex4/assets/js/script.js
+++ ex4/assets/js/script.js
@@ -1,14 +1,14 @@
 let imgSizing = document.querySelector('img');
 
-//console.log(this === imgSizing)
-
 imgSizing.onclick = () => {
-    let w = imgSizing.width;
-    console.log(imgSizing.width);
-    if (w > 1800) {
-        imgSizing.style.width = "50%";
-    } else {
+    
+    let w = imgSizing.style.width;
+
+    if (w === '50%') {
         imgSizing.style.width = "100%";
+    } else {
+        imgSizing.style.width = "50%";
     }
+    console.log(imgSizing.style.width);
 }
     
